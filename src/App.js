c5c7567d350ac7b029fdb71d6d7ac6a2fd240208
Rodrigo.Santos <rodrigo.santos-ext@dalenys.com>
2025-06-28 11:37:02 -0300

    add: first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        margin: "10px",
+        padding: "20px",
+        textAlign: "center",
+        backgroundColor: "#9C27B0",
+        color: "white",
+        borderRadius: "8px",
+        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+      }}
+    >
+      <h1>REMOTE APP 2</h1>
+      <h2>This is a new remote module in a separate repo</h2>
+    </div>
+  );
+};
+
+export default App;
